65d5bba53dca60304c1456d2c96c5f09a3d40709
Shivani Sharma <112687652+Skillbasket@users.noreply.github.com>
2023-11-23 20:31:13 +0530

    Delete Biometric evaluation directory

--- Biometric evaluation/BiometricSecurityScanner/databaseDelete.py
+++
@@ -1,7 +0,0 @@
-conn = sqlite3.connect('databaza.db')
-c = conn.cursor()
-conn.text_factory = str    
-data3 = str(input('Please enter name: '))
-query = "DELETE FROM Zoznam WHERE Name = '%s';" % data3.strip()
-print(query)
-mydata = c.execute(query)
